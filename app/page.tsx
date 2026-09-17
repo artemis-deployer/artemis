c820074e96dacc91899d7eeedb3e3303f2755a32
sifaq00 <asysyifaq25@gmail.com>
2026-09-17 21:53:44 +0700

    feat: add launch form, review dialog, studio page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,38 @@
-export default function Home() {
+"use client";
+
+import { useRef } from "react";
+import CharacterStage from "../components/CharacterStage";
+import { DraftProvider, useDraft } from "../components/DraftContext";
+import LaunchForm from "../components/LaunchForm";
+import ReviewDialog from "../components/ReviewDialog";
+import StatusBadge from "../components/StatusBadge";
+import StudioChat from "../components/StudioChat";
+import { CHAINS } from "../lib/chains";
+
+function Studio() {
+  const { draft } = useDraft();
+  const ref = useRef<HTMLDialogElement>(null);
+  const chain = CHAINS.find((c) => c.id === draft.chainId) ?? CHAINS[2];
   return (
     <main>
       <h1>Kentir</h1>
-      <p>Studio lands in Task 5.</p>
+      <StatusBadge />
+      <StudioChat />
+      <CharacterStage />
+      <LaunchForm
+        onReview={() => {
+          ref.current?.showModal();
+        }}
+      />
+      <ReviewDialog ref={ref} draft={draft} mainnet={!chain.testnet} />
     </main>
+  );
+}
+
+export default function Home() {
+  return (
+    <DraftProvider>
+      <Studio />
+    </DraftProvider>
   );
 }
